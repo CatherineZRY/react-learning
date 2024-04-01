5e47afb23f22eb02db69a7a9c80a3f12d2306c2e
CatherineZRY <14179795+catherine-zry@user.noreply.gitee.com>
2024-04-01 23:29:58 +0800

    transport parameter method

--- react-basic/src/Parent.js
+++ react-basic/src/Parent.js
@@ -1,14 +1,47 @@
+import { useState } from 'react'
 import Son from './Son'
 
 function Parent() {
   const childName = 'test1'
+  const [childMsg, setChildMsg] = useState('')
+  const [childMsg2, setChildMsg2] = useState('')
+  const getChildMsg = (msg) => {
+    setChildMsg(msg)
+  }
+  const getChildMsg2 = (msg) => {
+    setChildMsg2(msg)
+  }
   return (
     <div>
-      <span>This is parent</span>
-      <span>There is a son:</span>
-      <Son name={childName}
-        age={20}
-        cb={() => alert('this is a function')} />
+      <div>
+        <span>This is parent</span>
+      </div>
+      <div>
+        <span>There is a son:</span>
+      </div>
+      <div style={{ marginLeft: '16px' }}>
+        <Son name={childName}
+          age={20}
+          cb={() => alert('this is a function')}
+          onPostMsg={getChildMsg}
+          onPostMsg2={getChildMsg2}>
+          <span>child's child</span>
+        </Son>
+      </div>
+      {
+        childMsg &&
+        <div>
+          <span>This is child msg:</span>
+          <span>{childMsg}</span>
+        </div>
+      }
+      {
+        childMsg2 &&
+        <div>
+          <span>This is child msg2:</span>
+          <span>{childMsg2}</span>
+        </div>
+      }
     </div>
   )
 }
--- react-basic/src/Son.js
+++ react-basic/src/Son.js
@@ -1,14 +1,32 @@
+import { useState } from "react"
 
 // 父子间通信需要采用props参数进行接收，props参数接收到的时一个对象，其中包括了父组件传递的属性（例如：name）
 function Son(props) {
   // props是一个只读对象
+  const [childValue, setValue] = useState('')
+
   return (
     <div>
       <span>this is child</span>
       <span>My name is {props?.name ? props.name : '--'}. </span>
       <span>My age is {props?.age ? props.age : '--'}. </span>
       <button onClick={() => props.cb()}>click child</button>
-    </div>
+      <div>
+        {/* props.children是一个特殊的参数，可以从父组件传递出嵌套在子组件中的模板 */}
+        {props.children}
+      </div>
+      <div>
+        <label htmlFor="input1">input 1</label>
+        <input name="input1" value={childValue}
+          onChange={(e) => setValue(e.target.value)}></input>
+        <button onClick={() => props.onPostMsg(childValue)}> Post Msg to Parent </button>
+      </div>
+      <div style={{ marginTop: '16px' }}>
+        <label htmlFor="input2">input 2</label>
+        <input name="input2"
+          onChange={(e) => props.onPostMsg2(e.target.value)}></input>
+      </div>
+    </div >
   )
 }
 
