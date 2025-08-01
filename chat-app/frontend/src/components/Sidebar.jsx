82b89139f38f49fc955c7228c38655aebfce9b02
CatherineZRY <14179795+catherine-zry@user.noreply.gitee.com>
2025-08-02 00:16:51 +0800

    pull down and refresh user list

--- chat-app/frontend/src/components/Sidebar.jsx
+++ chat-app/frontend/src/components/Sidebar.jsx
@@ -1,22 +1,93 @@
 import { useChatStore } from '../store/useChatStore';
 import SidebarSkeleton from './skeletons/SidebarSkeleton';
-import { Users } from 'lucide-react';
+import { Users, Loader } from 'lucide-react';
 import { useAuthStore } from '../store/useAuthStore';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 function Sidebar() {
   const { users, getUsers, selectedUser, setSelectedUser, isUserLoading } = useChatStore();
 
   const { onlineUsers } = useAuthStore();
   const [showOnlineOnly, setShowOnlineOnly] = useState(false);
+  const filteredUsers = showOnlineOnly ? users.filter((user) => onlineUsers.includes(user._id)) : users;
+  const filterUserListRef = useRef(null);
+  const filterUserContainerRef = useRef(null);
+  const PULL_THRESHOLD = 50;      // 触发刷新的最小距离
+  const MAX_PULL_DISTANCE = 70;  // 最大下拉距离
+  const RESISTANCE_FACTOR = 0.6;  // 阻力系数（越小阻力越大）
+  const [pullState, setPullState] = useState({
+    isPulling: false,
+    startY: 0,
+    currentY: 0,
+    distance: 0,
+  });
+
+  const handleTouchStart = (e) => {
+    if (filterUserContainerRef.current.scrollTop === 0) {
+      setPullState({
+        isPulling: true,
+        startY: e.touches[0].clientY,
+        currentY: e.touches[0].clientY,
+        distance: 0,
+      })
+    }
+  }
+
+  const handleTouchMove = (e) => {
+    if (pullState.isPulling) {
+      // 更新移动状态
+      setPullState((lastState) => {
+        const distance = e.touches[0].clientY - lastState.startY;
+        // 需要移动的实际举例
+        const needTranslateY = Math.min(distance * RESISTANCE_FACTOR, MAX_PULL_DISTANCE);
+        translateUserListFromTop(needTranslateY);
+        return {
+          isPulling: true,
+          startY: lastState.startY,
+          currentY: e.touches[0].clientY,
+          distance: distance,
+        }
+      })
+
+    }
+  }
+
+  const translateUserListFromTop = (distance) => {
+    if (filterUserListRef.current) {
+      filterUserListRef.current.style.transform = `translateY(${distance}px)`;
+    }
+  }
+
+  const handleTouchEnd = () => {
+    // 重新置顶
+    setPullState((lastState) => {
+      if (lastState.isPulling) {
+        const distance = lastState.currentY - lastState.startY;
+        const actualDistance = distance * RESISTANCE_FACTOR;
+        checkCanRefreshUserList(actualDistance);
+      }
+      translateUserListFromTop(0);
+      return {
+        isPulling: false,
+        startY: 0,
+        currentY: 0,
+        distance: 0,
+      }
+    })
+  }
+
+  const checkCanRefreshUserList = (actualDistance) => {
+    if (actualDistance > PULL_THRESHOLD) {
+      getUsers();
+    }
+  }
+
 
   useEffect(() => {
     getUsers();
   }, [getUsers]);
 
-  const filteredUsers = showOnlineOnly ? users.filter((user) => onlineUsers.includes(user._id)) : users;
-
-  if (isUserLoading) {
+  if (isUserLoading && users.length === 0) {
     return <SidebarSkeleton />;
   }
 
@@ -27,7 +98,7 @@ function Sidebar() {
           <Users className="size-6"></Users>
           <span className='font-medium hidden lg:block'>Contacts</span>
         </div>
-        {/* TODO:online filter toggle */}
+        {/*  online filter toggle */}
         <div className="mt-3 hidden lg:flex items-center gap-2">
           <label className="cursor-pointer flex items-center gap-2">
             <input
@@ -42,40 +113,52 @@ function Sidebar() {
         </div>
       </div>
 
-      <div className="overflow-y-auto w-full py-3">
-        {filteredUsers.map((user) => (
-          <button
-            key={user._id}
-            onClick={() => setSelectedUser(user)}
-            className={`
+      <div className="overflow-y-auto w-full py-3" ref={filterUserContainerRef}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
+      >
+        <div ref={filterUserListRef}>
+          {
+            (pullState.isPulling || isUserLoading) &&
+            <div className="flex justify-center items-center h-7">
+              <Loader className={`w-10 h-10 ${isUserLoading ? "animate-spin" : ""}`} />
+            </div>
+          }
+          {filteredUsers.map((user) => (
+            <button
+              key={user._id}
+              onClick={() => setSelectedUser(user)}
+              className={`
               w-full p-3 flex items-center gap-3
               hover:bg-base-300 transition-colors
               ${selectedUser?._id === user._id ? "bg-base-300 ring-1 ring-base-300" : ""}
             `}
-          >
-            <div className="relative mx-auto lg:mx-0">
-              <img
-                src={user.profilePic || "/avatar.png"}
-                alt={user.name}
-                className="size-12 object-cover rounded-full"
-              />
-              {onlineUsers.includes(user._id) && (
-                <span
-                  className="absolute bottom-0 right-0 size-3 bg-green-500 
-                  rounded-full ring-2 ring-zinc-900"
+            >
+              <div className="relative mx-auto lg:mx-0">
+                <img
+                  src={user.profilePic || "/avatar.png"}
+                  alt={user.name}
+                  className="size-12 object-cover rounded-full"
                 />
-              )}
-            </div>
-
-            {/* User info - only visible on larger screens */}
-            <div className="hidden lg:block text-left min-w-0">
-              <div className="font-medium truncate">{user.fullName}</div>
-              <div className="text-sm text-zinc-400">
-                {onlineUsers.includes(user._id) ? "Online" : "Offline"}
+                {onlineUsers.includes(user._id) && (
+                  <span
+                    className="absolute bottom-0 right-0 size-3 bg-green-500 
+                  rounded-full ring-2 ring-zinc-900"
+                  />
+                )}
               </div>
-            </div>
-          </button>
-        ))}
+
+              {/* User info - only visible on larger screens */}
+              <div className="hidden lg:block text-left min-w-0">
+                <div className="font-medium truncate">{user.fullName}</div>
+                <div className="text-sm text-zinc-400">
+                  {onlineUsers.includes(user._id) ? "Online" : "Offline"}
+                </div>
+              </div>
+            </button>
+          ))}
+        </div>
 
         {onlineUsers.length === 0 && (
           <div className="text-center text-zinc-500 py-4">No online users</div>
